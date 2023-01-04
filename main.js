2a9b6ebec78cd146ea18e5405f41590594cb5999
diyor <232@ru>
2023-01-04 21:35:52 +0500

    master first save

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let a = +prompt('Введите число');
+let b = +prompt('Введите степень');
+let c = 1;
+for(let i =1; i <=b ; i++){
+b === 0 ===c;
+c = c * a;
+if(b<0){
+c = 1/a * c;    
+ }
+}
+   console.log(c);
